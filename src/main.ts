8428ac51c01d46ef1a1e14051886a42c3cf261a2
liangsuhao <2450381315@qq.com>
2022-01-09 15:27:58 +0800

    update db

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import {join} from 'path';
 
+const port = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public/upload'),{
     prefix: '/upload/',   //设置虚拟路径
  }); 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
